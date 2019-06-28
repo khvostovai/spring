3aeca10fa4becceb523df670c50a4a8d6277c5b9
kortez <fkortez@gmail.com>
2019-06-28 11:25:59 +0300

    изменение автризации пользователей через UserService

--- src/main/java/ru/kortez/test/controller/MainController.java
+++ src/main/java/ru/kortez/test/controller/MainController.java
@@ -1,12 +1,14 @@
 package ru.kortez.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.kortez.test.domain.Message;
+import ru.kortez.test.domain.User;
 import ru.kortez.test.repos.MessageRepository;
 
 @Controller
@@ -22,9 +24,9 @@ public class MainController {
         return "main";
     }
     @PostMapping("/main")
-    public String addMessage(@RequestParam String message, @RequestParam String tag, Model model){
+    public String addMessage(@AuthenticationPrincipal User user, @RequestParam String message, @RequestParam String tag, Model model){
         if (message != null && !message.equals("") )
-            messageRepository.save(new Message(message, tag));
+            messageRepository.save(new Message(message, tag, user));
         Iterable<Message> messages = messageRepository.findAll();
         model.addAttribute("messages", messages);
         return "main";
