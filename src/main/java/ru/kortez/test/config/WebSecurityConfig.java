99183e7711a69e7c26a6cb910ddbb86c68368af0
khvostovai <fkortez@gmail.com>
2019-07-03 18:18:05 +0300

    вход в редатор пользователя разрешен только для ADMIN

--- src/main/java/ru/kortez/test/config/WebSecurityConfig.java
+++ src/main/java/ru/kortez/test/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ru.kortez.test.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import ru.kortez.test.service.UserService;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
--- src/main/java/ru/kortez/test/controller/UserController.java
+++ src/main/java/ru/kortez/test/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.kortez.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Controller
+@PreAuthorize("hasAnyAuthority(ADMIN)")
 @RequestMapping("/user")
 public class UserController {
     @Autowired
@@ -37,7 +39,6 @@ public class UserController {
     public String userSave(@RequestParam("userId") User user,
                            @RequestParam("username") String username,
                            @RequestParam Map<String,String> form){
-        System.out.println("DONE");
         user.setUsername(username);
         Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
         user.getRoles().clear();
@@ -46,9 +47,7 @@ public class UserController {
                 user.getRoles().add(Role.valueOf(key));
             }
         }
-
         userRepository.save(user);
-
         return "redirect:/user";
 
     }
